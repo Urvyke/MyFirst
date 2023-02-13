4252bf29aec0f760312a505126f3c828b2f63564
“Urvyke” <541437975@qq.com>
2023-02-13 15:33:05 +0800

    docs: second

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/styles/index.scss'
+import SvgIcon from '@/icons'
+import 'element-plus/dist/index.css'
+import '@/router/permission'
+import * as ELIcons from '@element-plus/icons-vue'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+for (const iconName in ELIcons) {
+    app.component(iconName, ELIcons[iconName])
+}
+
+SvgIcon(app)
+app.use(store).use(router).mount('#app')
